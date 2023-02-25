28c5c59ad2e951d9a475b99cf777d9c5a1b579b2
Mohamadhossein4 <117532994+Mohamadhossein4@users.noreply.github.com>
2023-02-25 21:42:19 +0330

    .

--- Entrance.php
+++ Entrance.php
@@ -9,8 +9,16 @@
 if (isset($_POST['submit'])) {
     $a = $_POST['name'];
     $b = $_POST['last'];
+    $c= $_POST['grade'];
+    $d= $_POST['school'];
+    $e=$_POST['age'];
+    $f=$_POST['job'];
     $_SESSION['name'] = $a;
     $_SESSION['lastname'] = $b;
+    $_SESSION['grade'] = $c;
+    $_SESSION['school'] = $d;
+    $_SESSION['age'] = $e;
+    $_SESSION['job'] = $f;
     if (isset($_FILES['file'])) {
         $n = $_FILES['file']['name'];
         $tmp = $_FILES['file']['tmp_name'];
@@ -26,6 +34,14 @@ if (isset($_POST['submit'])) {
     <input type="text" name="name"><br>
     <label>نام خانوادگی خود را وارد کنید</label><br>
     <input type="text" name="last"><br>
+    <label>مقطع تحصیلی خود را وارد کنید</label><br>
+    <input type="number" name="grade"><br>
+    <label>نام هنرستان خود را وارد کنید</label><br>
+    <input type="text" name="school"><br>
+    <label>سن خود را وارد کنید</label><br>
+    <input type="number" name="age"><br>
+    <label>شغل خود را وارد کنید</label><br>
+    <input type="text" name="gob"><br>
     <input type="file" name="file">
     <input type="submit" name="submit" value="run">
 </form>
--- output.php
+++ output.php
@@ -2,9 +2,17 @@
     <?php session_start();
     echo "<tr>";
     echo "<tr>" . "<td>" . "first name:" . "</td>" . "<tr>";
-    echo "<tr>" . "<td>" . $_SESSION['name'] . " " . "</td>" . "<tr>";
+    echo "<tr>" . "<td>" . $_SESSION['name'] . "</td>" . "<tr>";
     echo "<tr>" . "<td>" . "last name:" . "</td>" . "<tr>";
     echo "<tr>" . "<td>" . $_SESSION['lastname'] . "</td>" . "<tr>";
+    echo "<tr>" . "<td>" . "age:" . "</td>" . "<tr>";
+    echo "<tr>" . "<td>" . $_SESSION['age'] . "</td>" . "<tr>";
+    echo "<tr>" . "<td>" . "grade:" . "</td>" . "<tr>";
+    echo "<tr>" . "<td>" . $_SESSION['grade'] . "</td>" . "<tr>";
+    echo "<tr>" . "<td>" . "school name:" . "</td>" . "<tr>";
+    echo "<tr>" . "<td>" . $_SESSION['school'] . "</td>" . "<tr>";
+    echo "<tr>" . "<td>" . "job:" . "</td>" . "<tr>";
+    echo "<tr>" . "<td>" . $_SESSION['job'] . "</td>" . "<tr>";
     $c = $_SESSION['picture'];
     echo "<tr>" . "<td>" . " image uploaded:" . "</td>" . "<tr>";
 
